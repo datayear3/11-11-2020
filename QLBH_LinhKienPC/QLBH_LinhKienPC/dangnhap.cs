2a9f63caeab3ac818128be389275c2f6a7850d80
Minh Châu <minhchau.music1999@gmail.com>
2020-11-11 16:22:48 +0700

    new

---
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBH_LinhKienPC
+{
+    public partial class dangnhap : Form
+    {
+        int dem = 0;
+        DAL.Lopdungchung lopchung = new DAL.Lopdungchung();
+        public dangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_dn_Click(object sender, EventArgs e)
+        {
+            string sql = "select count(*) from NHAN_VIEN where MaNV = '" + dn_tk.Text.ToUpper() + "'and MatKhau = '" + dn_mk.Text.ToUpper() + "'";
+            int kq = (int)lopchung.ExcuteScalar(sql);
+            if (kq >= 1)
+            {
+                GUI.Main main = new GUI.Main();
+                this.Hide();
+                main.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                dem++;
+                MessageBox.Show("Đăng nhập thất bại,mời bạn nhập lại");
+                if (dem == 3)
+                {
+                    MessageBox.Show("Bạn đã nhập sai 3 lần");
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
