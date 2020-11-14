0425f6d777f90591369571fb64edd738ccb441ac
Minh Châu <minhchau.music1999@gmail.com>
2020-11-14 10:54:54 +0700

    new

--- QLBH_LinhKienPC/QLBH_LinhKienPC/GUI/KHOHANG/khohang.Designer.cs
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/GUI/KHOHANG/khohang.Designer.cs
@@ -33,9 +33,10 @@ namespace QLBH_LinhKienPC.GUI.KHOHANG
             // 
             // khohang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "khohang";
             this.Size = new System.Drawing.Size(1620, 980);
             this.ResumeLayout(false);
--- QLBH_LinhKienPC/QLBH_LinhKienPC/GUI/Main.Designer.cs
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/GUI/Main.Designer.cs
@@ -58,9 +58,8 @@
             this.panel1.Controls.Add(this.bt_trangchu);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 1100);
+            this.panel1.Size = new System.Drawing.Size(300, 894);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -70,10 +69,9 @@
             this.bt_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bt_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_close.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_logout_rounded_down_64;
-            this.bt_close.Location = new System.Drawing.Point(109, 705);
-            this.bt_close.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_close.Location = new System.Drawing.Point(82, 573);
             this.bt_close.Name = "bt_close";
-            this.bt_close.Size = new System.Drawing.Size(133, 123);
+            this.bt_close.Size = new System.Drawing.Size(100, 100);
             this.bt_close.TabIndex = 0;
             this.bt_close.UseVisualStyleBackColor = true;
             this.bt_close.Click += new System.EventHandler(this.button7_Click);
@@ -83,9 +81,8 @@
             this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
             this.pictureBox1.Image = global::QLBH_LinhKienPC.Properties.Resources.logo_sell;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 123);
+            this.pictureBox1.Size = new System.Drawing.Size(300, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
@@ -97,10 +94,9 @@
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_home_page_50;
             this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button6.Location = new System.Drawing.Point(0, 591);
-            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Location = new System.Drawing.Point(0, 480);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(400, 86);
+            this.button6.Size = new System.Drawing.Size(300, 70);
             this.button6.TabIndex = 0;
             this.button6.Text = "TRANG CHỦ";
             this.button6.UseVisualStyleBackColor = true;
@@ -112,10 +108,9 @@
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_home_page_50;
             this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button5.Location = new System.Drawing.Point(4, 497);
-            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Location = new System.Drawing.Point(3, 404);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(392, 86);
+            this.button5.Size = new System.Drawing.Size(294, 70);
             this.button5.TabIndex = 0;
             this.button5.Text = "TRANG CHỦ";
             this.button5.UseVisualStyleBackColor = true;
@@ -128,10 +123,9 @@
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_home_page_50;
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button4.Location = new System.Drawing.Point(0, 404);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(0, 328);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(396, 86);
+            this.button4.Size = new System.Drawing.Size(297, 70);
             this.button4.TabIndex = 0;
             this.button4.Text = "TRANG CHỦ";
             this.button4.UseVisualStyleBackColor = true;
@@ -143,10 +137,9 @@
             this.bt_khohang.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_khohang.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_home_page_50;
             this.bt_khohang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bt_khohang.Location = new System.Drawing.Point(0, 310);
-            this.bt_khohang.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_khohang.Location = new System.Drawing.Point(0, 252);
             this.bt_khohang.Name = "bt_khohang";
-            this.bt_khohang.Size = new System.Drawing.Size(400, 86);
+            this.bt_khohang.Size = new System.Drawing.Size(300, 70);
             this.bt_khohang.TabIndex = 0;
             this.bt_khohang.Text = "KHO HÀNG";
             this.bt_khohang.UseVisualStyleBackColor = true;
@@ -159,10 +152,9 @@
             this.bt_banhang.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_banhang.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_sell_stock_50;
             this.bt_banhang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bt_banhang.Location = new System.Drawing.Point(0, 217);
-            this.bt_banhang.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_banhang.Location = new System.Drawing.Point(0, 176);
             this.bt_banhang.Name = "bt_banhang";
-            this.bt_banhang.Size = new System.Drawing.Size(396, 86);
+            this.bt_banhang.Size = new System.Drawing.Size(297, 70);
             this.bt_banhang.TabIndex = 0;
             this.bt_banhang.Text = "BÁN HÀNG";
             this.bt_banhang.UseVisualStyleBackColor = true;
@@ -175,10 +167,9 @@
             this.bt_trangchu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bt_trangchu.Image = global::QLBH_LinhKienPC.Properties.Resources.icons8_home_page_50;
             this.bt_trangchu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bt_trangchu.Location = new System.Drawing.Point(0, 123);
-            this.bt_trangchu.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_trangchu.Location = new System.Drawing.Point(0, 100);
             this.bt_trangchu.Name = "bt_trangchu";
-            this.bt_trangchu.Size = new System.Drawing.Size(400, 86);
+            this.bt_trangchu.Size = new System.Drawing.Size(300, 70);
             this.bt_trangchu.TabIndex = 0;
             this.bt_trangchu.Text = "TRANG CHỦ";
             this.bt_trangchu.UseVisualStyleBackColor = true;
@@ -188,50 +179,47 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(110)))), ((int)(((byte)(64)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(400, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Location = new System.Drawing.Point(300, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1540, 123);
+            this.panel2.Size = new System.Drawing.Size(1155, 100);
             this.panel2.TabIndex = 1;
             // 
             // us_banhang
             // 
             this.us_banhang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(165)))), ((int)(((byte)(149)))));
-            this.us_banhang.Location = new System.Drawing.Point(400, 123);
-            this.us_banhang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.us_banhang.Location = new System.Drawing.Point(300, 100);
             this.us_banhang.Name = "us_banhang";
-            this.us_banhang.Size = new System.Drawing.Size(2160, 1206);
+            this.us_banhang.Size = new System.Drawing.Size(1620, 980);
             this.us_banhang.TabIndex = 2;
             // 
             // us_trangchu
             // 
             this.us_trangchu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(240)))), ((int)(((byte)(225)))));
-            this.us_trangchu.Location = new System.Drawing.Point(400, 123);
-            this.us_trangchu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.us_trangchu.Location = new System.Drawing.Point(300, 100);
             this.us_trangchu.Name = "us_trangchu";
-            this.us_trangchu.Size = new System.Drawing.Size(2160, 1206);
+            this.us_trangchu.Size = new System.Drawing.Size(1620, 980);
             this.us_trangchu.TabIndex = 3;
             // 
             // us_khohang
             // 
             this.us_khohang.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.us_khohang.Location = new System.Drawing.Point(400, 123);
+            this.us_khohang.Location = new System.Drawing.Point(300, 100);
+            this.us_khohang.Margin = new System.Windows.Forms.Padding(2);
             this.us_khohang.Name = "us_khohang";
             this.us_khohang.Size = new System.Drawing.Size(1620, 980);
             this.us_khohang.TabIndex = 4;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1940, 1100);
+            this.ClientSize = new System.Drawing.Size(1455, 894);
             this.Controls.Add(this.us_khohang);
             this.Controls.Add(this.us_trangchu);
             this.Controls.Add(this.us_banhang);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Main";
             this.Text = "Main";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
