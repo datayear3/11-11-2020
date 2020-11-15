d9f92dc6cece9a79f96bc51e146d0ad2cf40a36b
Admin <ndgaming898@gmail.com>
2020-11-15 19:41:43 +0700

    new

--- QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
@@ -90,7 +90,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
