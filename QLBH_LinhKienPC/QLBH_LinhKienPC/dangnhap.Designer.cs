f1963edd8e8b0b30bb311755d6390b610574ccd7
wiza898 <71305075+wiza898@users.noreply.github.com>
2020-12-03 20:01:46 +0700

    doi vi tri

--- QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
@@ -113,7 +113,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::QLBH_LinhKienPC.Properties.Resources._512;
-            this.pictureBox2.Location = new System.Drawing.Point(334, 43);
+            this.pictureBox2.Location = new System.Drawing.Point(334, 152);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(73, 61);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -135,11 +135,7 @@
             this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-<<<<<<< HEAD
             this.label1.Location = new System.Drawing.Point(548, 43);
-=======
-            this.label1.Location = new System.Drawing.Point(435, 26);
->>>>>>> 41afc3ffaa9f43c294763e861260f4a47a2b5f6e
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(178, 58);
             this.label1.TabIndex = 6;
