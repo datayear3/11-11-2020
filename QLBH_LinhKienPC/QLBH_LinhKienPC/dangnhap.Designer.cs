41afc3ffaa9f43c294763e861260f4a47a2b5f6e
wiza898 <71305075+wiza898@users.noreply.github.com>
2020-12-03 19:53:53 +0700

    doi vi tri

--- QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
@@ -135,7 +135,7 @@
             this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(549, 44);
+            this.label1.Location = new System.Drawing.Point(435, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(178, 58);
             this.label1.TabIndex = 6;
