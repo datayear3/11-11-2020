38dbd40629a7f5fe1038b5fe19dde6bfbb32f7ec
wiza898 <71305075+wiza898@users.noreply.github.com>
2020-12-03 20:04:46 +0700

    login

--- QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
+++ QLBH_LinhKienPC/QLBH_LinhKienPC/dangnhap.Designer.cs
@@ -134,10 +134,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(548, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 58);
+            this.label1.Size = new System.Drawing.Size(234, 77);
             this.label1.TabIndex = 6;
             this.label1.Text = "LOGIN";
             // 
